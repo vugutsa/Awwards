75f2bb4c992dc9a77a659080fc95a8ff0bac94ea
vugutsa <vugutsamercy84@gmail.com>
2021-01-24 21:26:15 +0300

    import the Http404 function from django.http

--- awards/views.py
+++ awards/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
@@ -32,9 +32,12 @@ def convert_dates(dates):
     return day
 
 def past_days_awards(request,past_date):
+    try:
     # Converts data from the string Url
-    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
     day = convert_dates(date)
     html = f'''
         <html>
