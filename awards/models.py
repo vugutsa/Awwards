0b4d986740c44d7bfa60fe09ff5e06f7ffff0f31
vugutsa <vugutsamercy84@gmail.com>
2021-01-24 22:24:08 +0300

    create project and profile models

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length =60)
+    project_image = models.ImageField(upload_to = 'images/')
+    description = models.CharField(max_length =200)
+    link = models.URLField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+    
+class Profile(models.Model):
+    profile_image = models.ImageField(upload_to = 'images/')
+    user_bio = models.CharField(max_length =200)
+    contact_info = models.CharField(max_length=50, blank=False)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    
--- awww/settings.py
+++ awww/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'awww.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'moringa',
+    'PASSWORD':'Access',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
