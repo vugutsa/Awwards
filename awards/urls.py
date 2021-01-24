f9f975e0cec1c5872c0e2b62d30030ad19ce3182
vugutsa <vugutsamercy84@gmail.com>
2021-01-24 23:34:06 +0300

    import the project settings from the django.conf module and a static function

--- awards/urls.py
+++ awards/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     # url('^$',views.welcome,name = 'welcome'),
@@ -8,3 +9,5 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_awards,name = 'pastAwards')    
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
