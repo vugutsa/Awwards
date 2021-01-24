8aae93bd3662889fa756d0dace0e804c17e1fc21
vugutsa <vugutsamercy84@gmail.com>
2021-01-24 21:23:31 +0300

    create past_days_awards view function and pass in the request object and the date from the URL

--- awards/urls.py
+++ awards/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.awards_day,name='awardsToday')
+    url('^today/$',views.awards_day,name='awardsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_awards,name = 'pastAwards')
+    
 ]
--- awards/views.py
+++ awards/views.py
@@ -9,6 +9,9 @@ def welcome(request):
 
 def awards_day(request):
     date = dt.date.today()
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+
     html = f'''
         <html>
             <body>
@@ -27,3 +30,17 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
+def past_days_awards(request,past_date):
+    # Converts data from the string Url
+    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
